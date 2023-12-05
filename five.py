a41136b56485f03653ef3b385caad6b9cb133bd4
Geert Graat <g.graat@blueriq.com>
2023-12-05 16:04:46 +0100

    day five, part two

--- five.py
+++ five.py
@@ -17,6 +17,12 @@ class Map:
                 return r.get_destination(x)
         return x
 
+    def get_source(self, x):
+        for r in self.ranges:
+            if r.is_in_destination(x):
+                return r.get_source(x)
+        return x
+
 
 class Range:
     def __init__(self, destination, source, length):
@@ -27,8 +33,14 @@ class Range:
     def is_in_range(self, x):
         return self.source <= x < self.source + self.length
 
+    def is_in_destination(self, x):
+        return self.destination <= x < self.destination + self.length
+
     def get_destination(self, x):
         return self.destination + (x - self.source)
+
+    def get_source(self, x):
+        return self.source + (x - self.destination)
 
 
 class Five(Puzzle):
@@ -36,14 +48,24 @@ class Five(Puzzle):
         self.filename = filename
         self.seeds = []
         self.maps = []
+        self.read_map()
 
     def part_one(self):
-        self.read_map()
         locations = list(map(lambda s: self.follow_map(s), self.seeds))
         return min(locations)
 
     def part_two(self):
-        pass
+        ranges = []
+        maximum = 0
+        for start, length in zip(self.seeds[::2], self.seeds[1::2]):
+            ranges.append(range(start, start + length))
+            if start + length > maximum:
+                maximum = start + length
+        for location in range(maximum):
+            seed = self.reverse_map(location)
+            for r in ranges:
+                if seed in r:
+                    return location
 
     def read_map(self):
         for line in read_lines(self.filename):
@@ -63,6 +85,11 @@ class Five(Puzzle):
         for m in self.maps:
             s = m.get_destination(s)
         return s
+
+    def reverse_map(self, n):
+        for m in self.maps[::-1]:
+            n = m.get_source(n)
+        return n
 
 
 if __name__ == '__main__':
