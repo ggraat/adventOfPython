ed208ce551c122783f7d0a318b849ff514541682
Geert Graat <g.graat@blueriq.com>
2023-12-17 20:03:56 +0100

    day seventeen, part one

---
+++ seventeen.py
@@ -0,0 +1,71 @@
+from heapq import heappop, heappush
+from pathlib import Path
+from queue import PriorityQueue
+
+from common.puzzle import Puzzle
+from common.util import read_lines
+
+
+class Graph:
+    def __init__(self, _data):
+        self.weights = {}
+        for y, row in enumerate(_data):
+            for x, weight in enumerate(row):
+                node = (x, y)
+                self.weights[node] = int(weight)
+        self.width = len(_data[0])
+        self.height = len(_data)
+        self.end = (self.width - 1, self.height - 1)
+
+    def dijkstra(self):
+        todo = PriorityQueue()
+        todo.put((0, (0, 0), 0, 0))
+        todo.put((0, (0, 0), 1, 0))
+        visited = set()
+        dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+        while not todo.empty():
+            heat, current, direction, same_dir = todo.get()
+
+            if (current, direction, same_dir) in visited:
+                continue
+
+            visited.add((current, direction, same_dir))
+
+            if current == self.end:
+                return heat
+
+            x, y = current
+            dx, dy = dirs[direction]
+            if 0 <= x + dx < self.width and 0 <= y + dy < self.height:
+                next = (x + dx, y + dy)
+            else:
+                continue
+
+            heat += self.weights[next]
+
+            if same_dir < 2:
+                todo.put((heat, next, direction, same_dir + 1))
+
+            todo.put((heat, next, (direction + 1) % 4, 0))
+            todo.put((heat, next, (direction - 1) % 4, 0))
+        return -1
+
+
+class Seventeen(Puzzle):
+    def __init__(self, _data):
+        self.data = _data
+
+    def part_one(self):
+        graph = Graph(self.data)
+        return graph.dijkstra()
+
+    def part_two(self):
+        pass
+
+
+if __name__ == '__main__':
+    _data = read_lines(Path(__file__).stem)
+    puzzle = Seventeen(_data)
+    print(puzzle.part_one())
+    print(puzzle.part_two())
---
+++ tests/test_seventeen.py
@@ -0,0 +1,28 @@
+from unittest import TestCase
+
+from seventeen import Seventeen
+
+
+class TestSeventeen(TestCase):
+    _data = '''2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533'''.splitlines()
+
+    def setUp(self):
+        self.puzzle = Seventeen(self._data)
+
+    def test_part_one(self):
+        self.assertEqual(102, self.puzzle.part_one())
+
+    def test_part_two(self):
+        self.assertEqual(-1, self.puzzle.part_two())
